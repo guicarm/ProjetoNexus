19468b1c8690c5a00caf55ad9e3f0ea5e9c7d355
guicarm <rm98874@fiap.com.br>
2024-05-16 09:37:06 -0300

    add: securityConfig

---
+++ src/main/java/br/com/plusoft/nexus/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package br.com.plusoft.nexus.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain config(HttpSecurity http) throws Exception{
+
+        return http
+            .csrf( csrf -> csrf.disable() )
+            .build();
+    }
+    
+}
