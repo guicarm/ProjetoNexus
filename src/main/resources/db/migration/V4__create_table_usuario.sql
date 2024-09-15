b8f407ed3110d9faf4b0aaaff8cbca69d3b4b423
guicarm <RM98874@fiap.com.br>
2024-09-15 16:55:36 -0300

    fix: flyway

--- src/main/resources/db/migration/V4__create_table_usuario.sql
+++ src/main/resources/db/migration/V4__create_table_usuario.sql
@@ -5,5 +5,12 @@ CREATE TABLE T_NX_USUARIO (
     senha VARCHAR(255),    
     tipo VARCHAR(50),        
     PRIMARY KEY (id)       
-
 );
+
+INSERT INTO T_NX_USUARIO (nome, email, senha, tipo) 
+VALUES 
+('Amorgan', 'gansado@gmail.com', '$2a$10$hLe0htwGW66VaMk8jCp7xewxi2SKUof4shulI9K9Wxat1Z65nPcgO', 'cliente'),
+
+('Gustavo', 'godoi@gmail.com', '$2a$10$T1vtGIM6ghj0gq2DaPuAb.igUqQKsAZRnD4apV.uqzExD5jLZfwjK', 'cliente'),
+
+('Erick', 'erick@gmail.com', '$2a$10$z0cqcgahC/LNhr65D3Zmg.scw.X.BSkDQBAnJSIP9unQe.IZW718.', 'cliente');
